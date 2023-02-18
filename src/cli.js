b4a49a83f1d6caa811e8c6faaffaa748143b55ed
Luiz <gustavoooliveira111@gmail.com>
2023-02-18 09:55:06 -0300

    now, converting words to binary

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,9 @@
 const [argv] = process.argv.slice(2);
 
-if (isNaN(+argv)) return process.stdout.write("please type a number to convert.\n\n bitc -h for more help.\n");
+if (isNaN(+argv)) return process.stdout.write(argv.split('').map(letter => `${letter}: ${letter.codePointAt().toString(2)}`).join(' ')+"\n");
 
 if (argv === "-h" || argv === undefined) 
   return process.stdout.write("Usage: bitc [arguments]\n\nArguments:\n\n  -h      help.\n  number  convert your number to binary.\n");
 
 
-return process.stdout.write((+argv).toString(2)+"\n");
+return process.stdout.write(`${argv}: ${(+argv).toString(2)}\n`);
